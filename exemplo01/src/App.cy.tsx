d6c34835101258691bbf11f230b941ec62b95e27
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-02-24 19:10:11 -0400

    pro Rafael não ficar chateado

--- exemplo01/src/App.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import App from './App'
-
-describe('<App />', () => {
-  it('should render correct title', () => {
-    cy.mount(<App />)
-    cy.get('h1').should('have.text', 'Oi!')
-  })
-})
