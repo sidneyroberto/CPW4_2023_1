3159f0b479a69070b1b347dcc11d8205c005fb3f
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-03-02 20:10:17 -0400

    componentes finalizados

--- exemplo01/src/App.tsx
+++ exemplo01/src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Counter from './components/Counter'
 
 const App = () => {
+  const [value, setValue] = useState(0)
+
   return (
     <>
       <Header title='Exemplo Cypress' />
+
+      <Counter value={value} />
     </>
   )
 }
